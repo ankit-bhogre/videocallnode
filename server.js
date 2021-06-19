dda59976553b840acbcf9fa3148e2b879e81445c
Ankit Bhogre <tns.ankit@gmail.com>
2021-06-19 19:03:57 +0530

    changes

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-require('dotenv').config();
 const express = require("express");
 const http = require("http");
 const app = express();
 const server = http.createServer(app);
 const socket = require("socket.io");
 const io = socket(server);
-
+const port = process.env.PORT
 const users = {};
 
 const socketToRoom = {};
@@ -47,6 +46,9 @@ io.on('connection', socket => {
 
 });
 
-server.listen(process.env.PORT || 8080, () => console.log('server is running on port 8000'));
+//server.listen(process.env.PORT || 8000, () => console.log('server is running on port 8000'));
+server.listen(port,() => {
+    console.log(`Server running at port ${port}`);
+});
 
 
